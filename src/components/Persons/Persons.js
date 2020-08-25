734a68c52c9c326ce256e05c09c033549c75a8c7
teneons <teneons@gmail.com>
2020-08-25 16:54:14 +0300

    fix (class to className)

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react'
 export default class Persons extends Component {
     render() {
         return (
-            <ul class="list-group col-5 text-center m-2">
-                <li class="list-group-item list-group-item-action text-info">1</li>
-                <li class="list-group-item list-group-item-action text-info">2</li>
-                <li class="list-group-item list-group-item-action text-info">3</li>
+            <ul className="list-group col-5 text-center m-2">
+                <li className="list-group-item list-group-item-action text-info">1</li>
+                <li className="list-group-item list-group-item-action text-info">2</li>
+                <li className="list-group-item list-group-item-action text-info">3</li>
             </ul>
         )
     }
