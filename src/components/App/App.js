75b812f587b0302f4cfa5c9d8c3e4696b2703ef1
teneons <teneons@gmail.com>
2020-08-25 14:54:46 +0300

    make box persons

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Header from '../Header/Header';
 import Planets from '../Planets/Planets';
+import Persons from '../Persons/Persons';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Planets />
+      <Persons />
     </div>
   );
 }
---
+++ src/components/Persons/Persons.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Persons extends Component {
+    render() {
+        return (
+            <ul class="list-group col-5 text-center">
+                <li class="list-group-item list-group-item-action text-info">1</li>
+                <li class="list-group-item list-group-item-action text-info">2</li>
+                <li class="list-group-item list-group-item-action text-info">3</li>
+            </ul>
+        )
+    }
+}
