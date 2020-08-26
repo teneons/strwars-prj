b4204805957d021b95714b04481689261ba07317
teneons <teneons@gmail.com>
2020-08-26 14:02:32 +0300

    change save data to LS; add error handling

--- src/components/Planets/Planets.js
+++ src/components/Planets/Planets.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react'
 
 import SwapiData from '../SwapiData'
+import { buildQueries } from '@testing-library/react';
 //import Loader from './Loader'
 
 export default class Planets extends Component {
     state = {
-        loadStatus: true
+        d: null,
+        loadStatus: true,
+        catchEr: false
     }
     constructor() {
         super();
         this.planetsUpdate();
+    }
+
+    catchError = () => {
+        this.setState({catchEr: true, loadStatus: true})
     }
 
     swapiData = new SwapiData()
@@ -18,13 +25,9 @@ export default class Planets extends Component {
 
         this.swapiData.getPlanet(id).then(d => {
             this.setState({
-                id: d.id,
-                planetName: d.planetName,
-                diameter: d.diameter,
-                population: d.population,
-                orbitalPeriod: d.orbitalPeriod
-            })
-        }).then(() => this.setState({ loadStatus: false }))
+              d: d})
+        }).then(()=>console.log(this.state)).then(() => this.setState({ loadStatus: false }))
+            .catch(this.catchError)
     }
 
     render() {
@@ -33,6 +36,7 @@ export default class Planets extends Component {
             height: '60vh'
         }
 
+        //show loader
         if (this.state.loadStatus) {
             //<Loader />
             return (
@@ -42,22 +46,27 @@ export default class Planets extends Component {
             )
         }
 
+        //cath error
+        if(this.state.catchEr) {
+            return <h3>Error loading</h3>
+        }
+
         return (
             <div className="card border-info col-7 d-flex flex-row m-2" style={{ maxWidth: '55rem' }}>
                 <div className='col-6'>
-                    <img src={`https://starwars-visualguide.com/assets/img/planets/${this.state.id}.jpg`} className="card-img-top" style={stlImg} alt="Image planet" />
+                    <img src={`https://starwars-visualguide.com/assets/img/planets/${this.state.d.id}.jpg`} className="card-img-top" style={stlImg} alt="Image planet" />
                 </div>
                 <div className="card-body d-flex align-self-center flex-column text-uppercase">
-                    <h4 className="card-title font-weight-bold text-center text-info">{this.state.planetName}</h4>
+                    <h4 className="card-title font-weight-bold text-center text-info">{this.state.d.planetName}</h4>
                     <ul className="navbar-nav mr-auto" style={{ fontSize: '3.5vh' }}>
                         <li className="nav-item">
-                            <span className="nav-link" href="#">Diameter: <span className='font-weight-bold text-info'>{this.state.diameter}</span></span>
+                            <span className="nav-link" href="#">Diameter: <span className='font-weight-bold text-info'>{this.state.d.diameter}</span></span>
                         </li>
                         <li className="nav-item">
-                            <span className="nav-link" href="#">Population: <span className='font-weight-bold text-info'>{this.state.population}</span></span>
+                            <span className="nav-link" href="#">Population: <span className='font-weight-bold text-info'>{this.state.d.population}</span></span>
                         </li>
                         <li className="nav-item">
-                            <span className="nav-link" href="#">Orbital period: <span className='font-weight-bold text-info'>{this.state.orbitalPeriod}</span></span>
+                            <span className="nav-link" href="#">Orbital period: <span className='font-weight-bold text-info'>{this.state.d.orbitalPeriod}</span></span>
                         </li>
                     </ul>
                 </div>
