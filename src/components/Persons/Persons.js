9cc1b4e39d39532a57dd6fc6a0ca17ffe4d0679f
teneons <teneons@gmail.com>
2020-08-30 15:14:43 +0300

    finally make out items

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -9,39 +9,39 @@ export default class Persons extends Component {
         catchEr: false
     }
 
+    swapiData = new SwapiData()
+    updatePerson = () => {
+        this.swapiData.getAllPeoples()
+            .then((d)=> {this.setState({dataPersons: d})})
+            .catch(()=> this.setState({catchEr: !this.state.catchEr}))
+    }
+
     componentDidMount () {
         this.updatePerson()
     }
 
-    swapiData = new SwapiData()
-    updatePerson = () => {
-
-        this.swapiData.getAllPeoples()
-        .then((d)=> console.log(d))
-            //.then((d)=> {this.setState({dataPersons: d.peopleName})})
-            .catch(()=> this.setState({catchEr: !this.state.catchEr}))
-    }
-
-
     renderLi = (dataP) => {
-        return dataP.map((name)=>{
-            return(
-                <li className="list-group-item list-group-item-action text-info">{name}</li>
-            )
-        })
+        if(dataP) {
+        return dataP.map((nameData, id)=>{
+                return(
+                    <li className="list-group-item list-group-item-action text-info" key={id}>{nameData.personName}</li>
+                )
+            })
+        }
     }
 
     render() {
-
         if(this.state.catchEr) {
             return(
                 <h3>Error!</h3>
             )
         }
 
+        const li = this.renderLi(this.state.dataPersons)
+
         return (
             <ul className="list-group col-5 text-center m-2">
-                {this.renderLi}
+                {li}
             </ul>
         )
     }
