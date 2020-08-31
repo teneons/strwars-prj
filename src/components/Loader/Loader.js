c099074340e9b502976fb0f8a9badacef3c485e0
teneons <teneons@gmail.com>
2020-08-31 19:02:36 +0300

    make loader as separate component

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {ReactComponent as LoadSVG} from './ldr.svg'
+
+const Loader = () => {
+    return (
+        <div>
+            <LoadSVG />
+        </div>
+    )
+}
+
+export default Loader
