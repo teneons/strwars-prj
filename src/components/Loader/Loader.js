40873db09a061c0a6d16473aa3a0b0572723e8c8
teneons <teneons@gmail.com>
2020-09-01 23:00:46 +0300

    better loader

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -3,8 +3,10 @@ import {ReactComponent as LoadSVG} from './ldr.svg'
 
 const Loader = () => {
     return (
-        <div>
-            <LoadSVG />
+        <div className="card border-info col-7 d-flex flex-row m-2" style={{ maxWidth: '55rem' }}>
+            <div className="card-body d-flex justify-content-center flex-column text-uppercase">
+                <LoadSVG />
+            </div>
         </div>
     )
 }
