4c708b4392bc7ea74e5c0767e19f6d8726ca96ad
teneons <teneons@gmail.com>
2020-09-04 11:42:31 +0300

    reworked component for universal

--- src/components/Planets/Planets.js
+++ src/components/Planets/Planets.js
@@ -1,70 +1,13 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import SwapiData from '../SwapiData'
-import Loader from '../Loader/Loader';
+import CardBox from '../CardBox/CardBox'
 
-export default class Planets extends Component {
-    state = {
-        d: null,
-        loadStatus: true,
-        catchErr: false,
-        txtErr: null
-    }
-    constructor() {
-        super();
-        this.planetsUpdate();
-    }
+function Planets () {
+    const swapiD = new SwapiData()
+    const id = Math.floor(Math.random() * 21) + 2;  //gener id
 
-    catchError = () => {
-        this.setState({catchErr: true, loadStatus: false})
-    }
-
-    swapiData = new SwapiData()
-    planetsUpdate = () => {
-        const id = Math.floor(Math.random() * 21) + 2;
-
-        this.swapiData.getPlanet(id).then(d => {
-            this.setState({d: d})})
-            .then(() => this.setState({ loadStatus: false }))
-            .catch(this.catchError)
-    }
-
-    render() {
-        const stlImg = {
-            width: '60vh',
-            height: '60vh'
-        }
-
-        //show loader
-        if (this.state.loadStatus) {
-            return <Loader />
-        }
-
-        //cath error
-        if(this.state.catchErr) {
-            return <h3>Error</h3>
-        }
-
-        return (
-            <div className="card border-info col-7 d-flex flex-row m-2" style={{ maxWidth: '55rem' }}>
-                <div className='col-6'>
-                    <img src={`https://starwars-visualguide.com/assets/img/planets/${this.state.d.id}.jpg`} className="card-img-top" style={stlImg} alt="Image planet" />
-                </div>
-                <div className="card-body d-flex align-self-center flex-column text-uppercase">
-                    <h4 className="card-title font-weight-bold text-center text-info">{this.state.d.planetName}</h4>
-                    <ul className="navbar-nav mr-auto" style={{ fontSize: '3.5vh' }}>
-                        <li className="nav-item">
-                            <span className="nav-link" href="#">Diameter: <span className='font-weight-bold text-info'>{this.state.d.diameter}</span></span>
-                        </li>
-                        <li className="nav-item">
-                            <span className="nav-link" href="#">Population: <span className='font-weight-bold text-info'>{this.state.d.population}</span></span>
-                        </li>
-                        <li className="nav-item">
-                            <span className="nav-link" href="#">Orbital period: <span className='font-weight-bold text-info'>{this.state.d.orbitalPeriod}</span></span>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        )
-    }
+    return <CardBox swapiMethot={swapiD.getPlanet(id)} txtFields={['Diameter:', 'Population:', 'Orbital period:']} imgUrl={'https://starwars-visualguide.com/assets/img/planets/'} />
 }
+
+export default Planets
